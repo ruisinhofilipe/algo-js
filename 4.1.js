4d2780c59002eb118cd2a8ee3e3984b11e08ffe1
Rui Silva <ruisinhofilipe@gmail.com>
2023-06-08 11:16:46 +0200

    Exercise 2 of 4th section and added documentation to the functions

--- 4.1.js
+++ 4.1.js
@@ -1,5 +1,11 @@
 const readlineSync = require("readline-sync");
 
+/* 
+    Function that gets as input the length and width of triangle 
+    and calculates the area returning its value
+
+*/
+
 function calcSurface(l, w) {
   let area = (l * w) / 2;
   return `The area of your triangle is ${area}`;
---
+++ 4.2.js
@@ -0,0 +1,13 @@
+/*
+    Function that return a random number between 0 and 10
+    Math.random to get a random number between 0 and 1
+    * 10 so the random value won't be anymore between 0 and 1 but from 0 and 10
+
+    Math.round to round the number to the nearest number as a whole.
+*/
+
+function rand10() {
+  return Math.round(Math.random() * 10);
+}
+
+console.log(rand10());

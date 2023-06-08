31775254afa51c1f6bd8042a6a18b48a7736fb6b
Rui Silva <ruisinhofilipe@gmail.com>
2023-06-08 09:31:03 +0200

    3rd Exercise from the 2nd section

---
+++ 2.3.js
@@ -0,0 +1,11 @@
+// let i = 1;
+// while (i <= 100) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+for (let i = 1; i <= 100; i++) {
+  i % 2 === 0 ? console.log(i) : console.log("");
+}

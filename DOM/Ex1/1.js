0232e218b39ddaa6db16c0859081137e1a958e97
Rui Silva <ruisinhofilipe@gmail.com>
2023-06-13 14:57:15 +0200

    Ex7

--- DOM/Ex1/1.js
+++ DOM/Ex1/1.js
@@ -2,10 +2,20 @@ const title = document.querySelector("title");
 title.textContent = "Modifying the DOM";
 
 const body = document.querySelector("body");
-const bodyChildren = document.querySelectorAll("body *");
+// const bodyChildren = document.querySelectorAll("body *");
 
 body.style.backgroundColor = "red";
 
-bodyChildren.forEach((children) => {
-  console.log(children);
-});
+// bodyChildren.forEach((children) => {
+//   console.log(children);
+// });
+
+// for (let child of bodyChildren) {
+//   console.log(child);
+// }
+
+const bodyChildren = body.children;
+console.log(bodyChildren[1]);
+for (let child of bodyChildren) {
+  console.log(child);
+}
